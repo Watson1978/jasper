e957ffc6a728436adc63eb7c9e3ed3e18d4da784
Watson <watson1978@gmail.com>
2020-04-03 10:39:24 +0900

    Electron v8.2.0

--- script/linux/build-sqlite.sh
+++ script/linux/build-sqlite.sh
@@ -3,5 +3,5 @@ cd ./node_modules/sqlite3
 npm i nan@2.14.0
 npm i node-pre-gyp@0.10.1
 npm i -g node-gyp@3.8.0
-node-gyp configure --module_name=node_sqlite3 --module_path=../lib/binding/electron-v8.1-linux-x64
-node-gyp rebuild --target=8.1.1 --arch=x64 --target_platform=linux --dist-url=https://atom.io/download/electron --module_name=node_sqlite3 --module_path=../lib/binding/electron-v8.1-linux-x64
+node-gyp configure --module_name=node_sqlite3 --module_path=../lib/binding/electron-v8.2-linux-x64
+node-gyp rebuild --target=8.2.0 --arch=x64 --target_platform=linux --dist-url=https://atom.io/download/electron --module_name=node_sqlite3 --module_path=../lib/binding/electron-v8.2-linux-x64
